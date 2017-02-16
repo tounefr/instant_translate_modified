8156ae1ed3c812249436856d74378d4961cf28d9
Thomas HENON <thomas.henon@epitech.eu>
2017-02-16 13:24:47 +0100

    rm google analytics

--- src/kernel/init.js
+++ src/kernel/init.js
@@ -182,12 +182,14 @@
         },
 
         attachGA: function () {
+            /*
             var ga = document.createElement('script');
             ga.type = 'text/javascript';
             ga.async = true;
             ga.src = 'https://ssl.google-analytics.com/ga.js';
             var s = document.getElementsByTagName('script')[0];
             s.parentNode.insertBefore(ga, s);
+            */
         },
 
         getKeyByVal: function (o, v) {
